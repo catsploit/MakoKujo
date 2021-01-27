20ee59519fd1a08b61fbf652ed1cc59cff964fd5
catsploit <nicolas.schnitz@gmail.com>
2021-01-27 04:40:34 -0300

    modified mc command

--- mako.py
+++ mako.py
@@ -6,14 +6,23 @@ import discord
 import logging
 import re
 
+from config.commands.mc import mc_minehutscan
 from config.logger import Logger_custom
 from discord.ext import commands
 from urllib import request, parse
 
 # - Setting up - #
-bot = commands.Bot(command_prefix='.', description='Community bot made by @catsploit')
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='.', description='Community bot made by @catsploit', intents=intents)
 log = logging.getLogger('discord')
 log.setLevel(logging.WARNING)
+
+# - ch. Aliasses - #
+general = "¥-𝙜𝙚𝙣𝙚𝙧𝙖𝙡-¥"
+commands = "ß-𝙘𝙤𝙢𝙖𝙣𝙙𝙤𝙨-ß" 
+testing  = "testing-chat"
 
 # - Personal logger - #
 logger = Logger_custom()
@@ -34,9 +43,10 @@ async def stats(ctx):
 	stats_string = f"""```css
 	> {ctx.guild.name}'s stats
 	I=================================================I
-	[Server created at] => {ctx.guild.created_at}
-	[Owner]             => nick.sh
+	[Server created at] => {ctx.guild.created_at.strftime('%H:%M:%S %p')}
+	[Owner]             => {ctx.guild.owner}
 	[SERVER_ID]         => {ctx.guild.id}
+	[Members]           => {ctx.guild.member_count}
 	I=================================================I
 	```"""
 
@@ -49,30 +59,25 @@ async def yt(ctx, *, search):
 	html_response = request.urlopen('http://www.youtube.com/results?' + youtube_url)
 	youtube_results = re.findall(r'watch\?v=(\S{11})', html_response.read().decode())
 
+	logger.msg('INFO', f"searched '{search}' by {ctx.author}")
 	await ctx.send('https://www.youtube.com/watch?v=' + youtube_results[0])
 
 
 @bot.command()
-async def mc(ctx, server):
-	mc_info = request_api('https://api.minehut.com/server/', server, byname=0) #Minehut api request
-	try:
-		if mc_info['ok'] == False:
-			error = """```css
-			[request failed with api.minehut.com]```"""	
-			await ctx.send(error)
+async def mc(ctx, server, channels=[commands, testing, general]):
+	if str(ctx.channel) in channels:
+		author = ctx.author
+		mc_info = request_api('https://api.minehut.com/server/', server, author, byname=0)
 
-	except KeyError:
-		server_motd = str(mc_info['server']['motd'])
-		server_online = str(mc_info['server']['playerCount'])
+		try:
+			if mc_info['ok'] == False:
+				error = "`[request failed with api.minehut.com]`"	
+				await ctx.send(error)
 
-		response = f"""```css
-					[{server_motd:^10}]
-					I===========================================================I
-					Online players : {server_online}
-					I===========================================================I
-					```"""
-
-		await ctx.send(response)
+		except KeyError:
+			results = mc_minehutscan(mc_info)
+			await ctx.send(results[0])
+			await ctx.send(results[1])
 
 
 # - Other functions - #
@@ -81,11 +86,12 @@ def read_token(filename='../token.txt'): #1. REPLACE WITH YOUR TOKEN DIRECTORY
 	return token
 
 
-def request_api(url, arg, byname=1):
+def request_api(url, arg, author, byname=1):
 	keyword = ['?byName=true', '']
 
 	request = requests.get(url + arg + keyword[byname])
 	json_response = request.json()
+	logger.msg('INFO', f'requested {url} from {author}')
 
 	return json_response
 

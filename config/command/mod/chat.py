2182ca20953953fc720f0021fd9c0520dde8a4f3
catsploit <nicolas.schnitz@gmail.com>
2021-02-17 05:51:16 -0300

    added clean command

---
+++ config/command/mod/chat.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+#-*- coding:utf-8 -*-
+
+import discord
+from discord.ext import commands
+
+
+class Chat(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
+
+	#@commands.has_permissions(manage_messages=True)
+	@commands.command()
+	async def clear(self, ctx, amount : int):
+		await ctx.channel.purge(limit=amount)
+
+
+def setup(bot):
+	bot.add_cog(Chat(bot))
